1916c64eb751d9da0b3427b5d6a3aa6881a9ba0b
laiiihz <laiiihz@outlook.com>
2020-12-11 11:32:36 +0800

    update doc

--- lib/raw_toast.dart
+++ lib/raw_toast.dart
@@ -2,9 +2,20 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 
+///# RawToast
+///
+/// example:
+/// ```
+/// RawToast.toast('here your text')
+/// ```
 class RawToast {
   static const MethodChannel _channel = const MethodChannel('raw_toast');
 
+  ///make a Toast
+  /// example:
+  /// ```
+  /// RawToast.toast('here your text')
+  /// ```
   static Future toast(String text) async {
     final bool state = await _channel.invokeMethod('toast', {"text": text});
     return state;
